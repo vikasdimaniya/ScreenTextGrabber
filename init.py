81e267b21ff8f7655e7f67c119be41cc86dfa2e1
vikasdimaniya <vikaskumar6399@gmail.com>
2020-09-25 03:43:53 +0530

    Created OCR class

--- init.py
+++ init.py
@@ -1,66 +1,51 @@
-#IMPORT LIBS-----
+# IMPORT LIBS-----
 import cv2
 import numpy as np
-import pyautogui  
+import pyautogui
 import pytesseract
 from PIL import Image
 import time
+from ocr import ImageTextReader
 # from tkinter import *
 # import tkinter as tk
-
-#SETTINGS-----
-CHARACTER_BASED = False;
-time.sleep(1);
-#INPUT IMAGE-----
+IR = ImageTextReader()
+# SETTINGS-----
+# This means the OCR will search true->char by char, false->word by word
+CHARACTER_BASED = False
+# time.sleep(7)
+# INPUT IMAGE-----
 # img = cv2.imread("test2.png")
 img = pyautogui.screenshot()
 im = img
 px = im.load()
-# print (px[4, 4]) 
-#to set pixel value
-# px[4, 4] = (0, 0, 0) 
+# print (px[4, 4])
+# to set pixel value
+# px[4, 4] = (0, 0, 0)
 # cordinate = x, y = 4,4
-# # using getpixel method 
-# print (im.getpixel(cordinate)); 
+# # using getpixel method
+# print (im.getpixel(cordinate));
 
 img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-hImg, wImg,_ = img.shape
-original_img = img;
+hImg, wImg, _ = img.shape
+original_img = img
 
-#IMAGE FILTERING-----
-# img = cv2.resize(img,None, fx=0.5, fy=0.5)
-# img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2GRAY)
-# img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,9,1)
-# img = cv2.Canny(img,600,600)
-#Tesseract call-----
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# IMAGE FILTERING-----
+img = cv2.resize(img, None, fx=2, fy=2)
+img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2GRAY)
+# img = cv2.adaptiveThreshold(
+#    img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 1)
+#img = cv2.Canny(img, 600, 600)
+# Tesseract call-----
+
 # text=pytesseract.image_to_string(img,lang="eng")
 # print(text)
 if CHARACTER_BASED:
-    boxes = pytesseract.image_to_boxes(img)
-    for b in boxes.splitlines():
-        # print(b)
-        b = b.split(' ')
-        # print(b)
-        x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-        cv2.rectangle(img, (x,hImg- y), (w,hImg- h), (0, 0, 0), -1)
-        cv2.putText(img,b[0],(x,hImg- y),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),1)
+    original_img = IR.char_based(img)
 else:
-    boxes = pytesseract.image_to_data(img)
-    for a,b in enumerate(boxes.splitlines()):
-        # print(b)
-        if a!=0:
-            b = b.split()
-            if len(b)==12:
-                x,y,w,h = int(b[6]),int(b[7]),int(b[8]),int(b[9])
-                background_color=im.getpixel((x,y));
-                cv2.rectangle(img, (x,y), (x+w, y+h), background_color, -1)
-                # text_color=im.getpixel((x,y))
-                text_color=(255,255,255)
-                cv2.putText(original_img,b[11],(x,y+h),cv2.FONT_HERSHEY_SIMPLEX,0.7,text_color,1)
- 
-#DISPLAY IMAGE-----
-cv2.imshow("Img",original_img)
+    original_img = IR.word_based(img)
+
+# DISPLAY IMAGE-----
+cv2.imshow("Img", original_img)
 cv2.waitKey(0)
 
 
@@ -73,8 +58,8 @@ cv2.waitKey(0)
 # imga = pyautogui.screenshot()
 
 # def show_img():
-#     # take screenshot using pyautogui     
-#     img = cv2.cvtColor(np.array(imga), cv2.COLOR_RGB2BGR) 
+#     # take screenshot using pyautogui
+#     img = cv2.cvtColor(np.array(imga), cv2.COLOR_RGB2BGR)
 #     # img = cv2.resize(img, (400,400))
 #     # img = cv2.flip(img, 1)
 #     cv2image = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
--- ocr.py
+++ ocr.py
@@ -1,1 +1,47 @@
-# this file will contain all functions related to OCR
+import cv2
+import numpy as np
+import pyautogui
+import pytesseract
+from PIL import Image
+import time
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
+class ImageTextReader:
+    def filters(self, img):
+        img = cv2.resize(img, None, fx=2, fy=2)
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2GRAY)
+        return img
+
+    def char_based(self, img):
+        hImg, wImg, _ = img.shape
+        boxes = pytesseract.image_to_boxes(img)
+        for b in boxes.splitlines():
+            b = b.split(' ')
+            print(b[0])
+            x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+            cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (0, 0, 0), -1)
+            cv2.putText(img, b[0], (x, hImg - y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        return img
+
+    def word_based(self, img):
+        #img = self.filters(img)
+        im = Image.fromarray(np.uint8(img)).convert('RGB')
+        boxes = pytesseract.image_to_data(img)
+        for a, b in enumerate(boxes.splitlines()):
+            if a != 0:
+                b = b.split()
+                if len(b) == 12:
+                    print(b[11])
+                    x, y, w, h = int(b[6]), int(b[7]), int(b[8]), int(b[9])
+                    background_color = im.getpixel((x, y))
+                    cv2.rectangle(img, (x, y), (x+w, y+h),
+                                  background_color, -1)
+                    # text_color=im.getpixel((x,y))
+                    text_color = (255, 255, 255)
+                    cv2.putText(
+                        img, b[11], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
+                    # cv2.putText(img, str(x)+" "+str(y), (x, y+h),
+                    #             cv2.FONT_HERSHEY_SIMPLEX, 0.3, text_color, 1)
+        return img
